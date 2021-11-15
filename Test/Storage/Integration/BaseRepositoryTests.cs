bfd8a69e3b2dc3ddd5013cb96f69097ffda08f56
Alina Ying <alina.ying@gmail.com>
2021-11-15 12:05:49 -0800

    merge with unit tests PR

--- Test/Storage/Integration/BaseRepositoryTests.cs
+++ Test/Storage/Integration/BaseRepositoryTests.cs
@@ -1,109 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Common.StorageModels;
+using Management.StorageModels;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Primitives;
 using Moq;
 using Storage;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Test.Storage.Integration
 {
-    public class BaseRepositoryTests
+    public class BaseRepositoryTests : IDisposable
     {
         private readonly string _tableName = "user";
+        private readonly ITestOutputHelper _output;
+        private readonly MockRepository _mockRepo;
+        private readonly BaseRepository _repository;
+
+        public BaseRepositoryTests(ITestOutputHelper output)
+        {
+            this._output = output;
+            _mockRepo = new MockRepository(MockBehavior.Strict);
+            var iConfigMock = _mockRepo.Create<IConfiguration>();
+            iConfigMock.Setup(config => config.GetSection(It.IsAny<string>())).Returns(new Section());
+            _repository = new BaseRepository(iConfigMock.Object);
+            _repository.DeleteAllAsync(_tableName).Wait();
+            _repository.InsertAsync<User>(_tableName, new List<string>[]
+            {
+                new List<string>{ "TestInsert1", "2", "Admin",  DateTime.UtcNow.ToString(), "123" },
+            }).Wait();
+        }
+
+        public void Dispose()
+        {
+            _repository.DeleteAllAsync(_tableName).Wait();
+        }
 
         [Fact]
         public async Task GetAllAsync_ValidInput_Success()
         {
-            var mockRepo = new MockRepository(MockBehavior.Strict);
-            var iConfigMock = mockRepo.Create<IConfiguration>();
-            iConfigMock.Setup(config => config.GetSection(It.IsAny<string>())).Returns(new Section());
-
-            var baseRepo = new BaseRepository(iConfigMock.Object);
-            var result = await baseRepo.GetAllAsync<User>(_tableName);
-
+            var result = await _repository.GetAllAsync<User>(_tableName);
             Assert.NotEmpty(result);
-            mockRepo.VerifyAll();
+            _mockRepo.VerifyAll();
         }
 
         [Fact]
         public async Task GetAsync_ValidInput_Success()
         {
-            var mockRepo = new MockRepository(MockBehavior.Strict);
-            var iConfigMock = mockRepo.Create<IConfiguration>();
-            iConfigMock.Setup(config => config.GetSection(It.IsAny<string>())).Returns(new Section());
+            var result = await _repository.GetAsync<User>(_tableName, "UserId", "2");
 
-            var baseRepo = new BaseRepository(iConfigMock.Object);
-            var result = await baseRepo.GetAsync<User>(_tableName, "UserId", "1");
+            Assert.Equal("TestInsert1", result.UserName);
+            Assert.Equal("2", result.UserId);
+            Assert.Equal("Admin", result.UserRole);
+            Assert.Equal("123", result.CountryCode);
 
-            Assert.Equal("Muzhi", result.FullName);
-            Assert.Equal("1", result.UserId);
-            Assert.Equal("Admin", result.CountryCode);
-            mockRepo.VerifyAll();
+            _mockRepo.VerifyAll();
         }
 
         [Fact]
         public async Task InsertAsync_ValidInput_Success()
         {
-            var mockRepo = new MockRepository(MockBehavior.Strict);
-            var iConfigMock = mockRepo.Create<IConfiguration>();
-            iConfigMock.Setup(config => config.GetSection(It.IsAny<string>())).Returns(new Section());
-
-            var baseRepo = new BaseRepository(iConfigMock.Object);
-
-            await baseRepo.InsertAsync<User>(_tableName, new List<string>[]
+            await _repository.InsertAsync<User>(_tableName, new List<string>[]
             {
-                new List<string>{ "TestInsert1", "2", "Admin",  DateTime.UtcNow.ToString(), "123" },
+                new List<string>{ "TestInsert2", "3", "Admin",  DateTime.UtcNow.ToString(), "456" },
             });
 
-            var insertedData = await baseRepo.GetAsync<User>(_tableName, "UserId", "2");
+            var insertedData = await _repository.GetAsync<User>(_tableName, "UserId", "3");
             Assert.NotNull(insertedData);
 
-            await baseRepo.DeleteAsync<User>(_tableName, "UserId", "2");
-
-            mockRepo.VerifyAll();
+            _mockRepo.VerifyAll();
         }
 
         [Fact]
         public async Task UpdateAsync_ValidInput_Success()
         {
-            var mockRepo = new MockRepository(MockBehavior.Strict);
-            var iConfigMock = mockRepo.Create<IConfiguration>();
-            iConfigMock.Setup(config => config.GetSection(It.IsAny<string>())).Returns(new Section());
-
-            var baseRepo = new BaseRepository(iConfigMock.Object);
-
-            await baseRepo.UpdateAsync(_tableName, "UserId", "1", new Dictionary<string, string>
+            await _repository.UpdateAsync(_tableName, "UserId", "2", new Dictionary<string, string>
             {
                 { "UserRole", "Test" },
             });
 
-            var updatedResult = await baseRepo.GetAsync<User>(_tableName, "UserId", "1");
+            var updatedResult = await _repository.GetAsync<User>(_tableName, "UserId", "2");
 
-            Assert.Equal("Muzhi", updatedResult.FullName);
-            Assert.Equal("1", updatedResult.UserId);
-            Assert.Equal("Test", updatedResult.CountryCode);
+            Assert.Equal("TestInsert1", updatedResult.UserName);
+            Assert.Equal("2", updatedResult.UserId);
+            Assert.Equal("Test", updatedResult.UserRole);
 
-            await baseRepo.UpdateAsync(_tableName, "UserId", "1", new Dictionary<string, string>
-            {
-                { "UserRole", "Admin" },
-            });
-            mockRepo.VerifyAll();
+            _mockRepo.VerifyAll();
         }
     }
 
     public class Section : IConfigurationSection
     {
-        public string this[string key] { get => "Server=127.0.0.1; database=demo; UID=root; password=asesharp"; set => throw new NotImplementedException(); }
+        public string this[string key] { get => "Server=localhost; port=3306; database=asesharptestdb; UID=root; password=asesharp"; set => throw new NotImplementedException(); }
 
         public string Key => "ConnectionStrings";
 
         public string Path => throw new NotImplementedException();
 
-        public string Value { get => "server=127.0.0.1; database=demo; uid=root; pwd=asesharp"; set => throw new NotImplementedException(); }
-        // Data123!#
+        public string Value { get => "Server=localhost; port=3306; database=asesharptestdb; UID=root; password=asesharp"; set => throw new NotImplementedException(); }
 
         public IEnumerable<IConfigurationSection> GetChildren()
         {
