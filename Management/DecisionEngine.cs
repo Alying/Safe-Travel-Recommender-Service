fd2833c01ba15ccd8dce7e8b9787d7be75575d61
mli <mengwen9411@gmail.com>
2021-11-15 20:13:41 -0500

    Build fix.

--- Management/DecisionEngine.cs
+++ Management/DecisionEngine.cs
@@ -2,44 +2,37 @@
 //     Copyright (c) ASE#. All rights reserved.
 // </copyright>
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Management.DomainModels;
+using Management.Interface;
+
 namespace Management
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using Management.DomainModels;
-    using Management.Interface;
-
-    /// <summary>
-    /// Calculate the top 10 travel recommendations using weighted score
-    /// and get specific state's travel information
-    /// </summary>
     public class DecisionEngine : IDecisionEngine
     {
-        private readonly ICovidDataClient covidDataClient;     
-        private readonly IWeatherDataClient weatherDataClient;
-        private readonly IAirQualityDataClient airQualityDataClient;
+        private readonly ICovidDataClient _covidDataClient;
+        private readonly IWeatherDataClient _wheatherDataClient;
+        private readonly IAirQualityDataClient _airQualityDataClient;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DecisionEngine"/> class.
-        /// </summary>
-        /// <param name="covidDataClient">The COVID-19 data client.</param>
-        /// <param name="weatherDataClient">The weather data client.</param>
-        /// <param name="airQualityDataClient">The air quality data client.</param>
         public DecisionEngine(
             ICovidDataClient covidDataClient,
-            IWeatherDataClient weatherDataClient,
+            IWeatherDataClient wheatherDataClient,
             IAirQualityDataClient airQualityDataClient)
         {
-        
-        /// <summary>
-        /// Calculate the desired location using weighted scores from COVID-19, weather, and air quality
-        /// </summary>
-        /// <returns>The weighted score.</returns>
+            _covidDataClient = covidDataClient ?? throw new ArgumentNullException(nameof(covidDataClient));
+            _wheatherDataClient = wheatherDataClient ?? throw new ArgumentNullException(nameof(covidDataClient));
+            _airQualityDataClient = airQualityDataClient ?? throw new ArgumentNullException(nameof(airQualityDataClient));
+        }
+
         public Task<IEnumerable<Recommendation>> CalculateDesiredLocationAsync()
         {
-            var result = 0.3 * _covidDataClient.CalculateScoreAsync() + 0.3 * _weatherDataClient.CalculateScoreAsync() + 0.3 * _airQualityDataClient.CalculateScoreAsync();
+            var result = (0.3 * _covidDataClient.CalculateScoreAsync()) + (0.3 * _wheatherDataClient.CalculateScoreAsync()) + (0.3 * _airQualityDataClient.CalculateScoreAsync());
+
             return Task.FromResult(Enumerable.Empty<Recommendation>());
+        }
 
         /// <summary>
         /// Gets the specific location's information 
