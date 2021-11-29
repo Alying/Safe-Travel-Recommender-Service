de8a4fa8e9aa2eaeb8ca305a0daa107a9718fbf3
Alina Ying <alina.ying@gmail.com>
2021-11-28 23:34:39 -0800

    minimal client that talks with service

---
+++ Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientResponse
+    {
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        [JsonPropertyName("recommendationState")]
+        public string RecommendationState { get; set; }
+
+        [JsonPropertyName("overallScore")]
+        public float OverAllScore { get; set; }
+
+        [JsonPropertyName("airQualityScore")]
+        public float AirQualityScore { get; set; }
+
+        [JsonPropertyName("covidIndexScore")]
+        public float CovideIndexScore { get; set; }
+
+        [JsonPropertyName("weatherScore")]
+        public float WeatherScore { get; set; }
+    }
+
+    class Program
+    {
+        private static async Task<List<ClientResponse>> Client()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            string url = "https://localhost:5001/api/recommendations";
+            var streamTask = await client.GetStreamAsync(url);
+            Console.WriteLine(streamTask.ToString());
+            var responses = await System.Text.Json.JsonSerializer.DeserializeAsync<List<ClientResponse>>(streamTask);
+            return responses;
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting client...");
+
+            // start client with an enter after service is fully running
+            Console.ReadLine();
+
+            var task = await Client();
+            foreach (var itm in task)
+                Console.WriteLine($"{itm.State}, {itm.RecommendationState}, {itm.OverAllScore}");
+        }
+    }
+}
