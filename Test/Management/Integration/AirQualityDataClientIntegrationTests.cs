0aacfd20fa0efa942ee0ec7e3fc29b5dcbc86b35
Tian Liu <8xiaolong8@gmail.com>
2021-12-11 19:03:29 -0500

    Added Recommendation Test Cases

--- Test/Management/Integration/AirQualityDataClientIntegrationTests.cs
+++ Test/Management/Integration/AirQualityDataClientIntegrationTests.cs
@@ -26,12 +26,12 @@ namespace Test.Management.Integration
         {
             var mockRepo = new MockRepository(MockBehavior.Strict);
             var configMock = mockRepo.Create<IConfiguration>();
-
             configMock.Setup(config => config.GetSection(It.IsAny<string>())).Returns(new Section());
 
             var airClient = new AirQualityDataClient(configMock.Object);
 
-            var exception = await Assert.ThrowsAsync<Exception>(() => airClient.GetCityAirQualityDataAsync(City.Wrap("ase"), State.Wrap("California"), CountryCode.US, CancellationToken.None));
+            await Assert.ThrowsAsync<Exception>(() => airClient.GetCityAirQualityDataAsync(City.Wrap("ase"), State.Wrap("California"), CountryCode.US, CancellationToken.None));
+            await Assert.ThrowsAsync<Exception>(() => airClient.GetDefaultCitiesAsync(State.Wrap("ase"), CountryCode.US, CancellationToken.None));
 
             var resultME = await airClient.CalculateScoreForStateAsync(State.Wrap("ME"), CountryCode.US, CancellationToken.None);
             Assert.Equal("Maine", resultME.Item1.Value);
@@ -46,9 +46,9 @@ namespace Test.Management.Integration
             /// Gets api key
             /// </summary>
             /// <param name="key">api key string.</param>
-            public string this[string key] { get => "ecc93ce2-d18c-44a3-a414-adc270da84bd"; set => throw new NotImplementedException(); }
+            //public string this[string key] { get => "ecc93ce2-d18c-44a3-a414-adc270da84bd"; set => throw new NotImplementedException(); }
             
-            //public string this[string key] { get => "9ac310f1-7fb7-4ced-8808-478fdbe6ed13"; set => throw new NotImplementedException(); }
+            public string this[string key] { get => "9ac310f1-7fb7-4ced-8808-478fdbe6ed13"; set => throw new NotImplementedException(); }
             
             /// <summary>
             /// Gets connection string 
@@ -63,9 +63,9 @@ namespace Test.Management.Integration
             /// <summary>
             /// Gets or sets api key
             /// </summary>
-            public string Value { get => "ecc93ce2-d18c-44a3-a414-adc270da84bd"; set => throw new NotImplementedException(); }
+            //public string Value { get => "ecc93ce2-d18c-44a3-a414-adc270da84bd"; set => throw new NotImplementedException(); }
             
-            //public string Value { get => "9ac310f1-7fb7-4ced-8808-478fdbe6ed13"; set => throw new NotImplementedException(); }
+            public string Value { get => "9ac310f1-7fb7-4ced-8808-478fdbe6ed13"; set => throw new NotImplementedException(); }
 
             /// <summary>
             /// Gets the immediate descendant configuration sub-sections.
--- Test/Management/Unit/DecisionEngineTests.cs
+++ Test/Management/Unit/DecisionEngineTests.cs
@@ -1,13 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Management;
+using Management.DomainModels;
+using Management.Enum;
+using Management.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using Xunit;
 
-namespace Test.Management.Unit
+namespace Test.Management
 {
     /// <summary>
     /// Unit tests for decision engine
     /// </summary>
     public class DecisionEngineTests
     {
+        [Fact]
+        public async Task GetDefaultRecommendationAsync_AllGood_ReturnData()
+        {
+            var mockRepo = new MockRepository(MockBehavior.Strict);
+
+            var mockCovid = mockRepo.Create<ICovidDataClient>();
+            mockCovid.Setup(client => client.CalculateScoreForStateAsync(
+                                                It.IsAny<State>(),
+                                                It.IsAny<CountryCode>(),
+                                                It.IsAny<CancellationToken>())).Returns(Task.FromResult((State.Wrap("CA"), 50.0)));
+
+            var mockWeather = mockRepo.Create<IWeatherDataClient>();
+            mockWeather.Setup(client => client.CalculateScoreForStateAsync(
+                                               It.IsAny<State>(),
+                                               It.IsAny<CountryCode>(),
+                                               It.IsAny<CancellationToken>())).Returns(Task.FromResult((State.Wrap("CA"), 50.0)));
+
+            var mockAir = mockRepo.Create<IAirQualityDataClient>();
+            mockAir.Setup(client => client.CalculateScoreForStateAsync(
+                                               It.IsAny<State>(),
+                                               It.IsAny<CountryCode>(),
+                                               It.IsAny<CancellationToken>())).Returns(Task.FromResult((State.Wrap("CA"), 50.0)));
+
+            var decisionEngine = new DecisionEngine(mockCovid.Object, mockWeather.Object, mockAir.Object);
+            var result = await decisionEngine.GetDefaultCountryRecommendationAsync(CountryCode.US, CancellationToken.None);
+
+            var resultType = Assert.IsType<List<Recommendation>>(result);
+            Assert.NotNull(resultType[0]);
+        }
     }
 }
